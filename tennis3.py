3508cb6f6a9c2fd62c04bc1957e3ef1c2f769254
piolew24 <74770314+piolew24@users.noreply.github.com>
2026-04-14 18:40:30 +0200

    refactor tennis3.py

--- tennis3.py
+++ tennis3.py
@@ -1,27 +1,26 @@
 class TennisGame3:
     def __init__(self, player1_name, player2_name):
-        self.p1_n = player1_name
-        self.p2_n = player2_name
-        self.p1 = 0
-        self.p2 = 0
+        self.player1_name = player1_name
+        self.player2_name = player2_name
+        self.p1_points = 0
+        self.p2_points = 0
 
-    def won_point(self, n):
-        if n == "player1":
-            self.p1 += 1
+    def won_point(self, name):
+        if name == "player1":
+            self.p1_points += 1
         else:
-            self.p2 += 1
+            self.p2_points += 1
 
     def score(self):
-        if (self.p1 < 4 and self.p2 < 4) and (self.p1 + self.p2 < 6):
-            p = ["Love", "Fifteen", "Thirty", "Forty"]
-            s = p[self.p1]
-            return s + "-All" if (self.p1 == self.p2) else s + "-" + p[self.p2]
-        else:
-            if self.p1 == self.p2:
-                return "Deuce"
-            s = self.p1_n if self.p1 > self.p2 else self.p2_n
-            return (
-                "Advantage " + s
-                if ((self.p1 - self.p2) * (self.p1 - self.p2) == 1)
-                else "Win for " + s
-            )
+        point_names = ["Love", "Fifteen", "Thirty", "Forty"]
+        p1, p2 = self.p1_points, self.p2_points
+
+        if p1 == p2:
+            return f"{point_names[p1]}-All" if p1 < 3 else "Deuce"
+
+        if p1 >= 4 or p2 >= 4:
+            leader = self.player1_name if p1 > p2 else self.player2_name
+            return f"Advantage {leader}" if abs(p1 - p2) == 1 else f"Win for {leader}"
+
+        return f"{point_names[p1]}-{point_names[p2]}"
+
