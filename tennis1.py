974744e521534e165a46f9e5b969ce2d7901bba2
piolew24 <74770314+piolew24@users.noreply.github.com>
2026-04-14 18:02:36 +0200

    refactor tennis1.py

--- tennis1.py
+++ tennis1.py
@@ -2,45 +2,34 @@ class TennisGame1:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.p1points = 0
-        self.p2points = 0
+        self.p1_points = 0
+        self.p2_points = 0
 
     def won_point(self, player_name):
         if player_name == "player1":
-            self.p1points += 1
+            self.p1_points += 1
         else:
-            self.p2points += 1
+            self.p2_points += 1
 
     def score(self):
-        result = ""
-        temp_score = 0
-        if self.p1points == self.p2points:
-            result = {
-                0: "Love-All",
-                1: "Fifteen-All",
-                2: "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif self.p1points >= 4 or self.p2points >= 4:
-            minus_result = self.p1points - self.p2points
-            if minus_result == 1:
-                result = "Advantage player1"
-            elif minus_result == -1:
-                result = "Advantage player2"
-            elif minus_result >= 2:
-                result = "Win for player1"
-            else:
-                result = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.p1points
-                else:
-                    result += "-"
-                    temp_score = self.p2points
-                result += {
-                    0: "Love",
-                    1: "Fifteen",
-                    2: "Thirty",
-                    3: "Forty",
-                }[temp_score]
+        point_names = {
+            0: "Love",
+            1: "Fifteen",
+            2: "Thirty",
+            3: "Forty",
+        }
+        if self.p1_points == self.p2_points:
+            if self.p1_points < 3:
+                result = point_names[self.p1_points] + "-All"
+                return result
+            result = "Deuce"
+            return result
+        elif self.p1_points >= 4 or self.p2_points >= 4:
+            minus_result = self.p1_points - self.p2_points
+            if abs(minus_result) == 1:
+                result = "Advantage player1" if minus_result > 0 else "Advantage player2"
+                return result
+            result = "Win for player1" if minus_result > 0 else "Win for player2"
+            return result
+        result = point_names[self.p1_points] + "-" + point_names[self.p2_points]
         return result
